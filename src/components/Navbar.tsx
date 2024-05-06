b0ab9c0f51a043028e9a4a4640126de5e0f374ef
Nivrutha Balaji <nivrutha.balaji@inmation.com>
2024-05-06 13:52:19 +0200

    Changed logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 import { HStack, Image } from '@chakra-ui/react'
-import logo from '../assets/react.svg'
+import logo from '../assets/logo.png'
 import ColorModeSwitch from './ColorModeSwitch'
 import SearchInput from './SearchInput'
 
 const Navbar = () => {
     return (
-        <HStack paddingBottom={10} justifyContent={'space-between'}>
-            <Image src={logo} />
+        <HStack paddingBottom={10} justifyContent={'space-between'} height={'70px'}>
+            <Image width={'100px'} src={logo} />
             <SearchInput />
             <ColorModeSwitch />
         </HStack>
